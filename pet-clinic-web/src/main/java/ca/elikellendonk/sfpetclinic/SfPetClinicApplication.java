289d30b8bdf4d59c012da3c78db3d1c173646d32
Elias Kellendonk <elikellendonk@gmail.com>
2018-09-02 22:13:09 -0600

    Lazily init components on local env to improve dev performance.

--- pet-clinic-web/src/main/java/ca/elikellendonk/sfpetclinic/SfPetClinicApplication.java
+++ pet-clinic-web/src/main/java/ca/elikellendonk/sfpetclinic/SfPetClinicApplication.java
@@ -2,6 +2,9 @@ package ca.elikellendonk.sfpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class SfPetClinicApplication {
@@ -9,4 +12,9 @@ public class SfPetClinicApplication {
   public static void main(String[] args) {
     SpringApplication.run(SfPetClinicApplication.class, args);
   }
+
+  @Configuration
+  @Profile("local")
+  @ComponentScan(lazyInit = true)
+  static class LocalConfig {}
 }
