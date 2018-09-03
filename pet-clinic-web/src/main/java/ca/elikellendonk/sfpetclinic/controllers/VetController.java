a9a2d9df07c917e7c7b261ce63cc538c057bd380
Elias Kellendonk <elikellendonk@gmail.com>
2018-09-03 14:07:00 -0600

    Create Vet Index page and controller #7

---
+++ pet-clinic-web/src/main/java/ca/elikellendonk/sfpetclinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package ca.elikellendonk.sfpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+  @RequestMapping({"/vets", "/vets/", "/vets.html"})
+  public String index(Model model) {
+    return "vet-index";
+  }
+}
