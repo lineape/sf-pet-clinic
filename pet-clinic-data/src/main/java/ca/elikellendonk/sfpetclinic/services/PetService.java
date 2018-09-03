359e3b0e4303c31b26be428a0866c28e2e8c5e7b
Elias Kellendonk <elikellendonk@gmail.com>
2018-09-03 11:00:02 -0600

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/ca/elikellendonk/sfpetclinic/services/PetService.java
@@ -0,0 +1,12 @@
+package ca.elikellendonk.sfpetclinic.services;
+
+import ca.elikellendonk.sfpetclinic.model.Pet;
+import java.util.Set;
+
+public interface PetService {
+  Pet findById(Long id);
+
+  Pet save(Pet pet);
+
+  Set<Pet> findAll();
+}
