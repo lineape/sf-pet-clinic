141a57341b13439fc2590f549e7f8e43d0117b2b
Elias Kellendonk <elikellendonk@gmail.com>
2018-09-03 13:55:16 -0600

    Add Pet Clinic Index Page and Controller closes #8

---
+++ pet-clinic-web/src/main/java/ca/elikellendonk/sfpetclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package ca.elikellendonk.sfpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+  @RequestMapping({"/", "", "index", "index.html", "index.htm"})
+  public String index(Model model) {
+    return "index";
+  }
+}
