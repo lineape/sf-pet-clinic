ec51cfc4783655bc351605042821d3b4d48d77bf
Elias Kellendonk <elikellendonk@gmail.com>
2018-09-03 11:15:43 -0600

    Add base entity, closes #10

---
+++ pet-clinic-data/src/main/java/ca/elikellendonk/sfpetclinic/model/BaseEntity.java
@@ -0,0 +1,38 @@
+package ca.elikellendonk.sfpetclinic.model;
+
+import java.io.Serializable;
+
+public abstract class BaseEntity implements Serializable {
+  private Long id;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Boolean isNew() {
+    return id == null;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    BaseEntity that = (BaseEntity) o;
+
+    return isNew() ? id.equals(that.id) : that.isNew();
+  }
+
+  @Override
+  public int hashCode() {
+    return !isNew() ? id.hashCode() : 0;
+  }
+}
--- pet-clinic-data/src/main/java/ca/elikellendonk/sfpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/ca/elikellendonk/sfpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package ca.elikellendonk.sfpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
   private String firstName;
   private String lastName;
 
