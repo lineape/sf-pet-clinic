64d107f6186891fd1668235a5f1daf9299356ad6
Elias Kellendonk <elikellendonk@gmail.com>
2018-09-05 12:38:33 -0600

    Create Specialties service (map Impl) closes #24

---
+++ pet-clinic-data/src/main/java/ca/elikellendonk/sfpetclinic/services/SpecialtyService.java
@@ -0,0 +1,5 @@
+package ca.elikellendonk.sfpetclinic.services;
+
+import ca.elikellendonk.sfpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {}
--- pet-clinic-web/src/main/java/ca/elikellendonk/sfpetclinic/config/CrudServiceConfig.java
+++ pet-clinic-web/src/main/java/ca/elikellendonk/sfpetclinic/config/CrudServiceConfig.java
@@ -3,10 +3,12 @@ package ca.elikellendonk.sfpetclinic.config;
 import ca.elikellendonk.sfpetclinic.services.OwnerService;
 import ca.elikellendonk.sfpetclinic.services.PetService;
 import ca.elikellendonk.sfpetclinic.services.PetTypeService;
+import ca.elikellendonk.sfpetclinic.services.SpecialtyService;
 import ca.elikellendonk.sfpetclinic.services.VetService;
 import ca.elikellendonk.sfpetclinic.services.map.OwnerServiceMap;
 import ca.elikellendonk.sfpetclinic.services.map.PetServiceMap;
 import ca.elikellendonk.sfpetclinic.services.map.PetTypeServiceMap;
+import ca.elikellendonk.sfpetclinic.services.map.SpecialtyServiceMap;
 import ca.elikellendonk.sfpetclinic.services.map.VetServiceMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +19,6 @@ public class CrudServiceConfig {
   @Configuration
   @Profile({"local", "default"})
   static class CrudServiceMap {
-
     @Bean
     public OwnerService ownerService(PetService petService) {
       return new OwnerServiceMap(petService);
@@ -31,6 +32,11 @@ public class CrudServiceConfig {
     @Bean
     public PetTypeService petTypeService() {
       return new PetTypeServiceMap();
+    }
+
+    @Bean
+    public SpecialtyService specialtyService() {
+      return new SpecialtyServiceMap();
     }
 
     @Bean
