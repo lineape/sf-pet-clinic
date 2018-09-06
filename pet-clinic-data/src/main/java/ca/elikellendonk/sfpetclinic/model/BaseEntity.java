cc026c817c900e2d958cac8b06e25ec9c0e72bec
Elias Kellendonk <elikellendonk@gmail.com>
2018-09-06 10:15:26 -0600

    implement base entity (JPA) closes #28

--- pet-clinic-data/src/main/java/ca/elikellendonk/sfpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/ca/elikellendonk/sfpetclinic/model/BaseEntity.java
@@ -2,9 +2,16 @@ package ca.elikellendonk.sfpetclinic.model;
 
 import java.io.Serializable;
 import java.util.StringJoiner;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
+@MappedSuperclass
 public abstract class BaseEntity implements Serializable {
-  protected Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
   public Long getId() {
     return id;
